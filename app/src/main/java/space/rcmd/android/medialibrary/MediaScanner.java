vanilla-music/vanilla

change wake lock names as suggested by linter

app/src/main/java/ch/blinkenlights/android/medialibrary/MediaScanner.java
@@ -105,7 +105,7 @@ public class MediaScanner implements Handler.Callback {
 		HandlerThread handlerThread = new HandlerThread("MediaScannerThread", Process.THREAD_PRIORITY_LOWEST);
 		handlerThread.start();
 		mHandler = new Handler(handlerThread.getLooper(), this);
-		mWakeLock = ((PowerManager)context.getSystemService(Context.POWER_SERVICE)).newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "VanillaMusicIndexerLock");
+		mWakeLock = ((PowerManager)context.getSystemService(Context.POWER_SERVICE)).newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "VanillaMusic:Indexer");
 		mNotificationHelper = new NotificationHelper(context, NOTIFICATION_CHANNEL, context.getString(R.string.media_stats_progress));
 
 		// the content observer to use

app/src/main/java/ch/blinkenlights/android/vanilla/PlaybackService.java
@@ -487,7 +487,7 @@ public final class PlaybackService extends Service
 		mAutoPlPlaycounts = settings.getInt(PrefKeys.AUTOPLAYLIST_PLAYCOUNTS, PrefDefaults.AUTOPLAYLIST_PLAYCOUNTS);
 
 		PowerManager powerManager = (PowerManager)getSystemService(POWER_SERVICE);
-		mWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "VanillaMusicLock");
+		mWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "VanillaMusic:PlaybackService");
 
 		mReceiver = new Receiver();
 		IntentFilter filter = new IntentFilter();

reactorcoremeltdown/vanilla-plus

app/src/main/java/space/rcmd/android/medialibrary/MediaScanner.java
@@ -105,7 +105,7 @@ public class MediaScanner implements Handler.Callback {
 		HandlerThread handlerThread = new HandlerThread("MediaScannerThread", Process.THREAD_PRIORITY_LOWEST);
 		handlerThread.start();
 		mHandler = new Handler(handlerThread.getLooper(), this);
-		mWakeLock = ((PowerManager)context.getSystemService(Context.POWER_SERVICE)).newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "VanillaPlusIndexerLock");
+		mWakeLock = ((PowerManager)context.getSystemService(Context.POWER_SERVICE)).newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "VanillaPlus:Indexer");
 		mNotificationHelper = new NotificationHelper(context, NOTIFICATION_CHANNEL, context.getString(R.string.media_stats_progress));
 
 		// the content observer to use

app/src/main/java/space/rcmd/android/vanillaplus/PlaybackService.java
@@ -487,7 +487,7 @@ public final class PlaybackService extends Service
 		mAutoPlPlaycounts = settings.getInt(PrefKeys.AUTOPLAYLIST_PLAYCOUNTS, PrefDefaults.AUTOPLAYLIST_PLAYCOUNTS);
 
 		PowerManager powerManager = (PowerManager)getSystemService(POWER_SERVICE);
-		mWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "VanillaPlusLock");
+		mWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "VanillaPlus:PlaybackService");
 
 		mReceiver = new Receiver();
 		IntentFilter filter = new IntentFilter();